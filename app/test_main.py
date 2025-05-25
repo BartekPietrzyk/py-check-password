3f103719cd06e51ce3208c2c90f04927cb4e799a
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-05-25 17:11:09 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,13 @@
-# write your code here
+from .main import check_password
+
+
+def test_check_password_is_true() -> None:
+    assert check_password("Pass@word1") is True
+
+
+def test_check_password_is_too_short() -> None:
+    assert check_password("qwert") is False
+
+
+def test_check_password_is_too_long() -> None:
+    assert check_password("Pass@word123456789098765") is False
