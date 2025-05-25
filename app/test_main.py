6debadc56262d278a35d729f7eecfb69a96337d7
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-05-25 17:27:12 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,18 @@
+import pytest
 from .main import check_password
 
 
-def test_should_check_min_length() -> None:
-    assert check_password("qwert") is False
+@pytest.mark.parametrize(
+    "password, expected_result", [
+        ("Pass@word", False),
+        ("Password1", False),
+        ("pass@word1", False),
+        ("Pass@word1234567890", False),
+        ("Pass@1", False),
+        ("Pass@word1", True),
+    ]
+)
+def test_should_return_expected_result(password: str,
+                                       expected_result: bool) -> None:
 
-
-def test_should_check_upper_letter() -> None:
-    assert check_password("pass@word1") is False
-
-
-def test_should_check_max_length() -> None:
-    assert check_password("Pass@word123456789098765") is False
-
-
-def test_should_check_digit() -> None:
-    assert check_password("passW@ord") is False
-
-
-def test_should_check_special_symbols() -> None:
-    assert check_password("Password1") is False
+    assert check_password(password) == expected_result
