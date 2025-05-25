da2b1a11765410e7ce7f58ae5d3f166f6f435315
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-05-25 17:20:50 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,21 @@
 from .main import check_password
 
 
-def test_check_password_is_true() -> None:
-    assert check_password("Pass@word1") is True
-
-
-def test_check_password_is_too_short() -> None:
+def test_should_check_min_length() -> None:
     assert check_password("qwert") is False
 
 
-def test_check_password_is_too_long() -> None:
+def test_should_check_upper_letter() -> None:
+    assert check_password("pass@word1") is False
+
+
+def test_should_check_max_length() -> None:
     assert check_password("Pass@word123456789098765") is False
+
+
+def test_should_check_digit() -> None:
+    assert check_password("passW@ord") is False
+
+
+def test_should_check_special_symbols() -> None:
+    assert check_password("Password1") is False
